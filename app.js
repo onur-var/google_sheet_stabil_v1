695ce9515b5fc5111ec15c21c9d63594e43bebf4
onur-var <75581584+onur-var@users.noreply.github.com>
2024-11-22 23:20:38 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 const API_KEY = 'AIzaSyDltb5FbPvL9bLgj_GK4_DEDaPK0A7oM_g'; // Google Sheets API Key
-const SHEET_ID = '16XhSuD_8tEJ0wK_6H5f7csqIfsF6pFneNSphVb_6wsk';     // Google Sheet ID
-const RANGE = 'Sayfa1';                      // Sheet adı (genelde "Sheet1")
-
+const SHEET_ID = '16XhSuD_8tEJ0wK_6H5f7csqIfsF6pFneNSphVb_6wsk'; // Google Sheet ID
+const RANGE = 'Sayfa1'; // Sheet adı (genelde "Sheet1")
 
 async function fetchSheetData() {
     const url = `https://sheets.googleapis.com/v4/spreadsheets/${SHEET_ID}/values/${RANGE}?key=${API_KEY}`;
     const response = await fetch(url);
-if (!response.ok) {
-    console.error("Error fetching data:", response.status, response.statusText);
-    return [];
-}
+    if (!response.ok) {
+        console.error("Error fetching data:", response.status, response.statusText);
+        return [];
+    }
     const data = await response.json();
     return data.values;
 }
@@ -28,32 +27,26 @@ function convertDriveLinkToThumbnail(link) {
     }
 }
 
-// Kullanım örneği:
-const driveLink = "https://drive.google.com/uc?export=view&id=1otmBJ3vQVgLyvSQVKS5eZBmfJG_2FPmF";
-const thumbnailLink = convertDriveLinkToThumbnail(driveLink);
-console.log(thumbnailLink); // Çıktı: https://drive.google.com/thumbnail?id=1otmBJ3vQVgLyvSQVKS5eZBmfJG_2FPmF
-
-
 function populateTable(data) {
     const tbody = document.querySelector("#catalog-table tbody");
     tbody.innerHTML = ''; // Mevcut içeriği temizle
     data.slice(1).forEach(row => { // İlk satır başlıklarıdır
         const tr = document.createElement('tr');
-       row.forEach((cell, index) => {
-    const td = document.createElement('td');
-    if (index === 3) { // Image column
-        const img = document.createElement('img');
-        const thumbnailUrl = convertDriveLinkToThumbnail(cell);  // Linki dönüştür
-        img.src = thumbnailUrl;
-        console.log(thumbnailUrl); // Linki kontrol etmek için
-        img.alt = 'Catalog Image';
-        img.style.width = '100px';
-        td.appendChild(img);
-    } else {
-        td.textContent = cell;
-    }
-    tr.appendChild(td);
-});
+        row.forEach((cell, index) => {
+            const td = document.createElement('td');
+            if (index === 3) { // Image column
+                const img = document.createElement('img');
+                const thumbnailUrl = convertDriveLinkToThumbnail(cell);  // Linki dönüştür
+                img.src = thumbnailUrl;
+                console.log(thumbnailUrl); // Linki kontrol etmek için
+                img.alt = 'Catalog Image';
+                img.style.width = '100px';
+                td.appendChild(img);
+            } else {
+                td.textContent = cell;
+            }
+            tr.appendChild(td);
+        });
         tbody.appendChild(tr);
     });
 }
