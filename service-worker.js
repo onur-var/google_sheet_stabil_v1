dabc8e34c3f790e5e10c04ed3db2d4739b3bc785
onur-var <75581584+onur-var@users.noreply.github.com>
2024-11-23 23:15:51 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,14 @@ self.addEventListener('install', (event) => {
     console.log('Service Worker: Installed');
     event.waitUntil(
         caches.open('static-cache').then(cache => {
-            return cache.addAll(['./', './index.html', './style.css', './app.js']);
+            return cache.addAll([
+                './',
+                './index.html',
+                './style.css',
+                './app.js',
+                './icon-192.png',
+                './icon-512.png'
+            ]);
         })
     );
 });
